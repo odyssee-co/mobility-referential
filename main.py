5c319770fcde67476f8b5c78709925cfdf44f161
Matt <matthieu.mastio@gmail.com>
2023-07-07 18:13:55 +0200

    add grid splitting, car travel time matrix

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 #from IPython import embed; embed()
+import os
+os.environ['USE_PYGEOS'] = '0'
 import geopandas as gpd
-import os, sys
+import sys
 import yaml
 import utils
 import datetime
-from r5py import TransportNetwork, TravelTimeMatrixComputer, TransitMode, LegMode
-
+import numpy as np
+from itertools import product
+from shapely.geometry import Point
+from r5py import TransportNetwork, TravelTimeMatrixComputer, TransportMode
+import matplotlib.pyplot as plt
+from shapely.ops import nearest_points
 import osmnx as ox
+
 utn = ox.settings.useful_tags_node
 oxna = ox.settings.osm_xml_node_attrs
 oxnt = ox.settings.osm_xml_node_tags
@@ -27,11 +34,67 @@ def get_gdf():
     else:
         print("Processing zone...")
         muni = open(f"{data_path}/{municipalities_file}").read().split("\n")
-        gdf = gpd.read_file(f"{data_path}/communes.gpkg").to_crs(2154)
+        #gdf = gpd.read_file(f"{data_path}/communes.gpkg").to_crs(2154)
+        gdf = gpd.read_file(f"{data_path}/communes.gpkg").to_crs(4326)
         gdf = gdf[gdf["commune_id"].isin(muni)]
         gdf.reset_index(drop=True).to_feather(zone_path)
     return gdf
 
+def make_grid(gdf):
+    """
+    Compute a grid that cover uniformly the gdf shape
+    """
+    grid_path = f"{processed_path}/grid.feather"
+    if os.path.exists(grid_path):
+        print("Loading grid...")
+        grid = gpd.read_feather(grid_path)
+    else:
+        print("Processing grid...")
+        min_lon, min_lat, max_lon, max_lat = gdf.total_bounds
+        size = max(max_lon-min_lon, max_lat-min_lat)/grid_size
+        # compute the longitudes and latitudes top left corner coordinates
+        longitudes = np.arange(min_lon+size/2, max_lon, size)
+        latitudes = np.arange(min_lat+size/2, max_lat, size)
+        # create the grid centroids
+        points = []
+        for coords in product(longitudes, latitudes):
+            points.append(Point(coords[0], coords[1]))
+        points = gpd.GeoDataFrame({'geometry':points})
+        points.crs=4326
+        # clip to geometries
+        #grid = gpd.clip(gpd.GeoDataFrame({'geometry':points}, crs=2154), gdf)
+        grid = gpd.clip(points, gdf)
+        #grid = pd.DataFrame({"geometry":grid.geometry, "x":grid.geometry.x, "y":grid.geometry.y})
+        grid = grid.reset_index(drop=True)
+        grid["id"]=ox.distance.nearest_nodes(graph, grid.geometry.x,
+                                               grid.geometry.y, return_dist=False)
+        nodes, roads = ox.graph_to_gdfs(graph)
+        grid = grid[["id"]].merge(nodes[["geometry"]],
+                                                    left_on="id",
+                                                    right_on="osmid",
+                                                    how="left")
+        grid = gpd.GeoDataFrame(grid)
+        grid.reset_index(drop=True).to_feather(grid_path)
+    return grid
+
+
+def plot_grid(gdf, grid, graph):
+    nodes, roads = ox.graph_to_gdfs(graph)
+    ax = gdf.plot()
+    roads.plot(ax=ax, color="white", linewidth=1, alpha=0.2, zorder=3)
+    grid.plot(ax=ax, color="red", markersize=5, zorder=10)
+    ax.set_axis_off()
+    plt.show()
+
+def get_grid_id(grid, x, y):
+    """
+    Return the nearest grid centroid from the x, y coordinates of a point
+    """
+    geometry = gpd.GeoSeries(gpd.points_from_xy(grid.x, grid.y))
+    g = grid.copy()
+    g["geometry"] = geometry
+    queried, nearest = nearest_points(Point(x, y), geometry.unary_union)
+    return int(grid[g.geometry == nearest].index[0])
 
 def get_network(gdf):
     if os.path.exists(path_graphml):
@@ -42,9 +105,9 @@ def get_network(gdf):
         area = gdf.dissolve().to_crs(4326)
         #cf = '["highway"~"motorway|trunk|primary|secondary"]'
         graph = ox.graph_from_polygon(area.geometry[0], network_type="all",
-                                      retain_all=True, truncate_by_edge=True,
-                                      clean_periphery=True)
-        graph = ox.projection.project_graph(graph, to_crs=2154)
+                                      retain_all=False, truncate_by_edge=True,
+                                      clean_periphery=False)
+        #graph = ox.projection.project_graph(graph, to_crs=2154)
         ox.save_graphml(graph, filepath=path_graphml)
     if not os.path.exists(path_osm):
         ox.save_graph_xml(graph, filepath=path_osm)
@@ -61,6 +124,7 @@ if __name__ == "__main__":
     data_path = cfg["data_path"]
     municipalities_file = cfg["municipalities_file"]
     gtfs_file = cfg["gtfs_file"]
+    grid_size = cfg["grid_size"]
 
     #Create the processed data directory
     processed_path = f"{data_path}/processed/{municipalities_file.split('.')[0]}"
@@ -75,19 +139,19 @@ if __name__ == "__main__":
 
     gdf = get_gdf()
     graph = get_network(gdf)
+    grid = make_grid(gdf)
+
+    #plot_grid(gdf, grid, graph)
 
     nodes, roads = ox.graph_to_gdfs(graph)
     if not os.path.exists(path_pbf):
         utils.osm_to_pbf(path_osm, path_pbf)
 
-    from IPython import embed; embed()
-    """
     transport_network = TransportNetwork(path_pbf, [gtfs_file])
+
     travel_time_matrix_computer = TravelTimeMatrixComputer(
         transport_network,
-        origins=station,
-        destinations=pop_points,
-        departure=datetime.datetime(2023,1,19,7,30),
-        transport_modes=[TransitMode.TRANSIT, LegMode.WALK])
-    travel_time_matrix = travel_time_matrix_computer.compute_travel_times()
-    """
+        origins=grid,
+        transport_modes=[TransportMode.CAR])
+    car_tt = travel_time_matrix_computer.compute_travel_times()
+    car_tt.reset_index(drop=True).to_feather(f"{processed_path}/car_tt.feather")
